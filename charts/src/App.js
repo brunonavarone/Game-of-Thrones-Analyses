3dbe3067f0393fe1ecd5392fad2532c9d31c4b9f
marcosjudd <marcosantonio788@hotmail.com>
2023-03-07 14:55:27 -0300

    enviando parte da estilização

--- charts/src/App.js
+++ charts/src/App.js
@@ -5,14 +5,35 @@ import Columnchart2 from "./components/Columnchart2/index.jsx";
 import Columnchart from "./components/Columnchart/index.jsx";
 import Piechart from "./components/Piechart/index.jsx";
 
+import img1 from "./img/gotlogoprateada.png"
+import img2 from "./img/got-footer.svg"
+import img3 from "./img/basededados.png"
+
 function App() {
   return (
     <>
-      <Columnchart />
-      <Barchart />
-      <Columnchart2 />
-      <Piechart />
-      <Radarchart />      
+
+    <div className="main_container">  
+      <div className="gotlogo"> 
+        <img src={img1} alt="Logo" />
+      </div>
+      <h1>BASE DE DADOS</h1>
+      <div className="container_perguntas">
+      </div>
+    </div>
+
+      <div className="graficos">
+        <Columnchart />
+        <Barchart />
+        <Columnchart2 />
+        <Piechart />
+        <Radarchart />      
+      </div>
+
+    <footer>
+      <img src={img2} alt="Logo" />
+    </footer>
+
     </>
   );
 }
